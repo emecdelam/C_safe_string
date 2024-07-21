61f5da2b407ecb8b240f226f338d518ea4e1ab5a
emecdelam <kiwi.thefruit0000@gmail.com>
2024-07-21 21:15:17 +0200

    removing unused define in .h

--- safe_string.h
+++ safe_string.h
@@ -4,7 +4,9 @@
 #define STRING_MALLOC_FAIL (void *) -1
 #define STRING_OVERFLOW -1
 #define STRING_SUCCESS 0
-#define STRING_INVALID_INPUT 1
+
+
+
 typedef struct {
     char *buffer;
     size_t max_length;
